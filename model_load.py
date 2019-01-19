2b06fe1def57d8028bf316318625a5c33766e6db
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-01-19 16:52:46 +0900

    "auto-commit by kronos"

--- model_load.py
+++ model_load.py
@@ -1,7 +1,5 @@
 import torch
 
-try:
-    model = torch.load('./pretrained_model/coco_resnet_50_map_0_335.pt')
-    print('Load Success')
-except:
-    print('Load Fail')
+model = torch.load('./pretrained_model/coco_resnet_50_map_0_335.pt')
+
+print('Load Success')
