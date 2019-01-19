ae0918873b80ad18b7679758b17c63c58ee05ea1
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-01-19 16:44:03 +0900

    change the paths in build.sh

--- lib/build.sh
+++ lib/build.sh
@@ -7,9 +7,13 @@ CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
 
 
 # Build NMS
-cd nms/src/cuda
+# cd nms/src/cuda
+# echo "Compiling nms kernels by nvcc..."
+# /usr/local/cuda/bin/nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC $CUDA_ARCH
+# cd ../../
+# python build.py
+# cd ../
+
 echo "Compiling nms kernels by nvcc..."
-/usr/local/cuda/bin/nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC $CUDA_ARCH
-cd ../../
-python build.py
-cd ../
+/usr/local/cuda/bin/nvcc -c -o ./lib/nms/src/cuda/nms_kernel.cu.o ./lib/nms/src/cuda/nms_kernel.cu -x cu -Xcompiler -fPIC $CUDA_ARCH
+python ./lib/nms/build.py
